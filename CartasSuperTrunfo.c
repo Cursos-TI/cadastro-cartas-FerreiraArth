738b8b2484aa6b5c2199c766e193ee4ccf3efd24
FerreiraArth <122117492+FerreiraArth@users.noreply.github.com>
2026-01-23 17:53:49 +0000

    aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,6 +12,8 @@
     float area1;
     float pib1;
     int pontos1;
+    float densidade1;
+    float pibPerCapita1;
 
     /* ===== CARTA 2 ===== */
     char estado2;
@@ -21,6 +23,8 @@
     float area2;
     float pib2;
     int pontos2;
+    float densidade2;
+    float pibPerCapita2;
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
@@ -53,6 +57,9 @@ int main() {
     printf("Numero de pontos turisticos: ");
     scanf("%d", &pontos1);
 
+    densidade1 = populacao1 / area1;
+    pibPerCapita1 = pib1 / populacao1;
+
      /* ===== ENTRADA CARTA 2 ===== */
     printf("\n=== Cadastro da Carta 2 ===\n");
 
@@ -77,6 +84,8 @@ int main() {
     printf("Numero de pontos turisticos: ");
     scanf("%d", &pontos2);
 
+    densidade2 = populacao2 / area2;
+    pibPerCapita2 = pib2 / populacao2;
 
     /* ===== SAÍDA DOS DADOS ===== */
     printf("==============================\n");
@@ -88,6 +97,8 @@ int main() {
     printf("Area: %.2f km²\n", area1);
     printf("PIB: %.2f bilhoes de reais\n", pib1);
     printf("Numero de Pontos Turisticos: %d\n", pontos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
 
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
@@ -97,6 +108,8 @@ int main() {
     printf("Area: %.2f km²\n", area2);
     printf("PIB: %.2f bilhoes de reais\n", pib2);
     printf("Numero de Pontos Turisticos: %d\n", pontos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
 
 return 0;
 } 
